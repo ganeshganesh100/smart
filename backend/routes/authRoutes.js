7a51399e98c4f5a89d4267ba0a0da0b3dedb1743
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-06-25 17:32:57 +0545

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
 const upload = require('../middleware/uploadMiddleware');
 
-router.post('/register', upload.fields([
-    { name: 'profilePicture', maxCount: 1 },
-    { name: 'citizenshipDoc', maxCount: 1 }
-]), registerUser);
 
 router.post('/login', loginUser);
 
