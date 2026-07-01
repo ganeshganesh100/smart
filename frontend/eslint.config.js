f2e2db9d2e89784e3fb8926e4bd3ca1608532d50
Ganesh Devkota <152066208+ganeshganesh100@users.noreply.github.com>
2026-07-01 18:49:20 +0545

    Update ESLint config to include additional file types

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+
     extends: [
     
       reactHooks.configs.flat.recommended,
